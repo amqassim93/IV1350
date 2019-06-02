3b8f8064c3ed471c0eb210207f8e1d033f2cad42
Ahmad Qassim <ahmad.qassimalasmar@gmail.com>
2019-06-02 22:57:11 +0200

    sem 4

--- src/se/kth/iv1350/saleproccess/model/Subject.java
+++
@@ -1,7 +0,0 @@
-package se.kth.iv1350.saleproccess.model;
-
-public interface Subject {
-    public void regis(TotalObserver display);
-    public void notifyObserver();
-    public void addNewPrice(Amount amt);
-}
